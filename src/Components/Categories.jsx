dbeb8aee54e403c50427a4f6e9522b3bcadf7d9a
rhems234 <rhems234@gmail.com>
2023-06-08 10:29:28 +0900

    Test Commit

---
+++ src/Components/Categories.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+const categories = [
+  { name: "food", text: "부산맛집정보 서비스" },
+  { name: "walking", text: "부산도보여행 정보 서비스" },
+  { name: "festival", text: "부산 축제 정보 서비스" },
+  { name: "theme", text: "부산 테마여행 정보 서비스" },
+];
+
+const CategoriesBlock = styled.div`
+  display: flex;
+  padding: 1rem;
+  width: 768px;
+  margin: 0 auto;
+  @media scren and (max-width: 768px) {
+    width: 100%;
+    overflow-x: auto;
+  }
+`;
+
+const Category = styled(NavLink)`
+  font-size: 1.2 rem;
+  cursor: pointer;
+  white-space: pre;
+  text-decoration: none;
+  color: inherit;
+  padding-bottom: 0.2rem;
+
+  &.hover {
+    color: #495057;
+  }
+
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #cf2225;
+    color: #22cf61;
+    &:hover {
+      color: #dbce3b;
+    }
+  }
+
+  & + & {
+    margin-left: 5rem;
+  }
+`;
+
+const Categories = () => {
+  return (
+    <CategoriesBlock>
+      {categories.map((c) => (
+        <Category
+          key={c.name}
+          className={({ isActive }) => (isActive ? "active" : undefined)}
+          to={c.name === "food" ? "/" : `/${c.name}`}
+        >
+          {c.text}
+        </Category>
+      ))}
+    </CategoriesBlock>
+  );
+};
+
+export default Categories;
